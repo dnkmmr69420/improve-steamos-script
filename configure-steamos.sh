5f4af1eba737ed07c6df285876a1abab84bae50b
dnkmmr <104856296+dnkmmr69420@users.noreply.github.com>
2024-03-05 10:52:08 -0600

    Update configure-steamos.sh

--- configure-steamos.sh
+++ configure-steamos.sh
@@ -2,15 +2,14 @@
 
 # this sets up steamos in my prefered way
 
-# use flatpak user instead of flatpak system
+# adds flatpak user
 echo "Switching to flatpak user..."
 flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo --user
-flatpak remote-delete flathub --system
-flatpak remove --all --system
 
 # Configure flatpak
 echo "Configuring flatpak..."
 flatpak override --user --filesystem=xdg-config/gtk-3.0:ro
+flatpak override --system --filesystem=xdg-config/gtk-3.0:ro
 
 # Configure distrobox
 echo "Configuring ~/.distroboxrc..."
@@ -28,8 +27,8 @@ chmod +x ~/.local/bin/neofetch
 
 # Install some user flatpaks
 echo "Installing flatpaks..."
-flatpak install flathub org.mozilla.firefox --user --or-update -y
-flatpak install flathub org.kde.kcalc --user --or-update -y
-flatpak install flathub org.kde.kclock --user --or-update -y
+flatpak install flathub org.mozilla.firefox --system --or-update -y
+flatpak install flathub org.kde.kcalc --system --or-update -y
+flatpak install flathub org.kde.kclock --system --or-update -y
 
 
